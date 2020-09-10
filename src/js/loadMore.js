36533f4e57eb1ddf57d4d03c3a529cd159062723
Jarsey <jarseymini@gmail.com>
2020-09-10 14:31:07 +0800

    下拉获取数据滚动实现修改

--- src/js/loadMore.js
+++ src/js/loadMore.js
@@ -1,19 +1,36 @@
 // 浏览器可视窗口顶端距离网页顶端的高度（垂直偏移）
 import schoolList from './index.js'
 import $ from 'jquery'
-let clientHeight = $(document).height() // 整个文档高度
+const documentElement = document.documentElement
+const documentBody = document.body
+// 获取滚动条当前的位置
+function getScrollTop () {
+  var scrollTop = 0
+  var elementScrollTop = documentElement.scrollTop
+  var bodyScrollTop = documentBody.scrollTop
+  if (documentElement && elementScrollTop) {
+    scrollTop = elementScrollTop
+  } else if (documentBody) {
+    scrollTop = bodyScrollTop
+  }
+  return scrollTop
+}
+// 获取当前可视范围的高度
+function getClientHeight () {
+  var elementClientHeight = documentElement.clientHeight
+  var bodyClientHeight = documentBody.clientHeight
+  var clientHeight = bodyClientHeight && elementClientHeight ? Math.min(bodyClientHeight, elementClientHeight) : Math.max(bodyClientHeight, elementClientHeight)
+  return clientHeight
+}
 
-const windowHeight = $(window).height() // 浏览器可视窗口高度
+// 获取文档完整的高度
+function getScrollHeight () {
+  return Math.max(documentBody.scrollHeight, documentElement.scrollHeight)
+}
 
-let scrollTop = $(window).scrollTop() // 初始化滚动条位置
-// $('.school-section').scrollTop(0)
-
+// 滚动事件触发
 $(window).unbind('scroll').bind('scroll', () => {
-  scrollTop = $(window).scrollTop()
-  clientHeight = $(document).height()
-  console.log('schoolList', schoolList.schoolList.total)
-
-  if (scrollTop + windowHeight + 35 > clientHeight && schoolList.schoolList.total < 50) {
+  if (getScrollTop() + getClientHeight() >= getScrollHeight() && schoolList.schoolList.total < 50) {
     $.get('http://localhost:5371/api/list', (data, status) => {
       if (status === 'success' && data.status === 0) {
         schoolList.schoolList.appendItem(data.msg.data)
